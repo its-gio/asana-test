09f35c32342f11b5fba675ae366f596c9247d52f
its-gio <me@its-g.io>
2018-12-06 16:00:16 -0800

    Adding forEach to fetch to pass animal array to new funciton to add image to ul

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,17 @@
+// import * as data from "../data/dogs.json";
 const animals = [];
-fetch("../data/dogs.json")
-  .then(blob => blob.json())
-  .then(data => animals.push(...data.dogs));
-
 const list = document.getElementById("pet-list");
+
+(async function getAnimals() {
+  await fetch("../data/dogs.json")
+    .then(blob => blob.json())
+    .then(data => animals.push(...data.dogs));
+  animals.forEach(animal => showAnimal(animal));
+})();
+
+function showAnimal(animal) {
+  console.log(animal);
+  //   let li = document.createElement("li");
+  //   let img = document.createElement("img");
+  //   list.appendChild(li);
+}
